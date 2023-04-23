14fb91cab99f608bf5aeace4b9870d249b976626
tt <teppoeva@gmail.com>
2023-04-22 22:47:12 -0700

    Update train.py

--- train.py
+++ train.py
@@ -106,7 +106,7 @@ def run(rank, n_gpus, hps):
         eval_dataset = TextAudioLoaderWithDuration(hps.data.validation_files, hps.data)
         eval_loader = DataLoader(
             eval_dataset,
-            num_workers=8,
+            num_workers=2,
             shuffle=False,
             batch_size=hps.train.batch_size,
             pin_memory=False,
